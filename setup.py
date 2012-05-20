4fdad04f91898511fff316cfe5fe33bbe9065e97
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-05-20 18:20:48 +0300

    Added long description to setup.py, changed version

--- setup.py
+++ setup.py
@@ -1,14 +1,24 @@
+import os
 from setuptools import setup, find_packages
+
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')
+description = "A django application that contains a widget to render a \
+    form field as beautiful Imperavi WYSIWYG editor http://redactorjs.com/"
+
+if os.path.exists(README_PATH):
+    long_description = open(README_PATH).read()
+else:
+    long_description = description
 
 setup(
     name="django-imperavi",
-    version="0.1.1",
+    version="0.1.2",
     author="Vasyl Stanislavchuk",
     author_email="vasyl.stanislavchuk@gmail.com",
     packages=find_packages(),
     include_package_data=True,
-    description="A django application that contains a widget to render a" \
-        "form field as beautiful Imperavi WYSIWYG editor http://redactorjs.com/",
+    description=description,
+    long_description=long_description,
     license="MIT License",
     keywords="django admin widget imperavi",
     platforms=['any'],
