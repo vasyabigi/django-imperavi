8c71833a6c44427354f6f4eb44160607e92a8d1b
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-06-16 14:13:17 +0300

    Release 0.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-imperavi",
-    version="0.1.6",
+    version="0.1.7",
     author="Vasyl Stanislavchuk",
     author_email="vasyl.stanislavchuk@gmail.com",
     packages=find_packages(),
