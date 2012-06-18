94e807fc209b019e6bfbe7a334c1dd100d25b77c
Vasyl Stanislavchuk <vasyl.stanislavchuk@djangostars.com>
2012-06-19 00:00:23 +0300

    Fixed bug with README.rst

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-imperavi",
-    version="0.2.1",
+    version="0.2.2",
     author="Vasyl Stanislavchuk",
     author_email="vasyl.stanislavchuk@gmail.com",
     packages=find_packages(),
