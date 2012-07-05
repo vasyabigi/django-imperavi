bc75fb51890cabff6451172c3c7cfbc285836c65
Guilherme Gondim <semente@taurinus.org>
2012-07-05 17:32:10 -0300

    remove unused imports

--- imperavi/widget.py
+++ imperavi/widget.py
@@ -1,9 +1,6 @@
 import json
 from django.forms.widgets import Textarea
-from django.forms.util import flatatt
 from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape
-from django.utils.encoding import force_unicode
 from django.core.urlresolvers import reverse
 from django.conf import settings
 
