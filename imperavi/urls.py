e140751248675229734a7141d3f3f224fccfa2d1
Patrick Craston <patrick@craston.com>
2016-09-05 13:28:18 +0100

    update urls to not use patterns in order to work latest Django

--- imperavi/urls.py
+++ imperavi/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
-from views import upload_image, uploaded_images_json, upload_file
+from .views import upload_image, uploaded_images_json, upload_file
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^upload-image/(?P<upload_path>.*)', upload_image, name="imperavi-upload-image"),
     url(r'^get-json/(?P<upload_path>.*)', uploaded_images_json, name="imperavi-get-json"),
     url(r'^upload-file/(?P<upload_path>.*)', upload_file, name="imperavi-upload-file"),
     url(r'^upload-link-file/(?P<upload_path>.*)', upload_file, {'upload_link': True}, name="imperavi-upload-link-file"),
-)
+]
